05e17b4d4e6f042c15a7f543d208457ff9c0e8c3
Upendra Pratap Kushwaha <upendra.kushwaha@stigasoft.com>
2017-10-13 19:22:00 +0530

    added app key and password

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "b37d7371-7db5-410f-8b47-50dab50f9a30",
+    appPassword: "aev950CtMVnufBQ9P499dxJ"
 });
 
 // Listen for messages from users 

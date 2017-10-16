2921282594ca4b8c21746de6adcf18ca2c0b3d61
Upendra Pratap Kushwaha <upendra.kushwaha@stigasoft.com>
2017-10-16 12:32:01 +0530

    make process varibales

--- index.js
+++ index.js
@@ -12,10 +12,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 });
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: "b37d7371-7db5-410f-8b47-50dab50f9a30",
-    appPassword: "aev950CtMVnufBQ9P499dxJ"
- //appId: process.env.MICROSOFT_APP_ID,
- //appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
 // If a Post request is made to /api/messages on port 3978 of our local server, then we pass it to the bot connector to handle

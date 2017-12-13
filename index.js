84bab52faec8362892c082857f958c0d3ad43098
Upendra Pratap Kushwaha <upendrapratap001@gmail.com>
2017-12-13 17:58:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,5 +18,6 @@ server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+   console.log("You said: %s", session.message.text)
+    session.send("Noted");
 });
